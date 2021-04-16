4fcbf18a3bc1a7d4c295b5faac7a4f6370b52b7a
mick xu <mick@holdcode.com>
2021-04-16 13:32:15 +0800

    Article 组件

---
+++ examples/components/Article.vue
@@ -0,0 +1,26 @@
+<template>
+  <article class="article-contianer">
+    <h1>{{ title }}</h1>
+    <slot />
+  </article>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: "",
+    },
+  },
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.article-contianer {
+  padding-left: 46px;
+}
+</style>
