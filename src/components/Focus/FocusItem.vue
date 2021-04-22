7e30de569553652db91b278fd6262b88dd55452c
mick xu <mick@holdcode.com>
2021-04-22 13:44:10 +0800

    支持手动播放和自动播放

--- src/components/Focus/FocusItem.vue
+++ src/components/Focus/FocusItem.vue
@@ -1,18 +1,25 @@
 <template>
-  <div class="focus-item" v-show="display"><slot /></div>
+  <div :class="classes"><slot /></div>
 </template>
 
 <script>
-import { toRefs, watchEffect } from "vue";
+import { computed, onMounted, getCurrentInstance, inject } from "vue";
+
 export default {
   name: "FocusItem",
   props: {
-    visible: { type: Boolean, default: false },
+    title: { type: String, default: "" },
   },
-  setup(props) {
-    const { display } = toRefs(props);
-    watchEffect(() => {});
-    return { display };
+  setup() {
+    const focusScope = inject("FocusScope");
+    const instance = getCurrentInstance();
+    const classes = computed(() => {
+      return ["focus-item", { ["active"]: focusScope.data.activeUid === instance.uid }];
+    });
+    onMounted(() => {
+      focusScope.addItem(instance.uid);
+    });
+    return { classes };
   },
 };
 </script>
--- src/components/Focus/Index.vue
+++ src/components/Focus/Index.vue
@@ -1,31 +1,67 @@
 <template>
   <div :class="classes" :style="styles">
-    <component :is="item" :key="index" v-for="(item, index) in items" />
-    <div class="focus-control focus-prev">‹</div>
-    <div class="focus-control focus-next">›</div>
+    <!-- <component :is="item" :key="index" v-for="(item, index) in items" /> -->
+    <slot />
+    <div class="focus-control focus-prev" @click="prev">‹</div>
+    <div class="focus-control focus-next" @click="next">›</div>
   </div>
 </template>
 
 <script>
-import { computed, onMounted, reactive, nextTick } from "vue";
+import { computed, onMounted, reactive, ref, provide, onUnmounted } from "vue";
 export default {
   name: "Focus",
-  props: {},
-  setup(props, { slots }) {
+  props: {
+    autoPlay: { type: Boolean, default: true },
+  },
+  setup(props) {
     const classes = computed(() => {
       return ["focus"];
     });
-    const items = slots.default();
-    const styles = reactive({ height: "0px" });
-    let index = 0;
 
-    items[index].props.visible = true;
-    onMounted(() => {
-      nextTick(() => {
-        styles.height = `${items[index].children.default()[0].el.clientHeight}px`;
-      });
+    const data = reactive({
+      activeUid: 0,
+      timer: undefined,
     });
-    return { classes, items, styles };
+
+    const items = ref([]);
+    const addItem = (item) => {
+      items.value.push(item);
+    };
+
+    const play = () => {
+      data.timer = setInterval(() => {
+        next();
+      }, 3000);
+    };
+
+    const styles = reactive({ height: "100px" });
+    const prev = () => {
+      let index = items.value.indexOf(data.activeUid);
+      index = index > 0 ? index : items.value.length;
+      data.activeUid = items.value[--index];
+    };
+    const next = () => {
+      let index = items.value.indexOf(data.activeUid);
+      index = index < items.value.length - 1 ? index : -1;
+      data.activeUid = items.value[++index];
+    };
+
+    onMounted(() => {
+      data.activeUid = items.value[0];
+      props.autoPlay && play();
+    });
+
+    onUnmounted(() => {
+      clearInterval(data.timer);
+    });
+
+    provide("FocusScope", {
+      data,
+      addItem,
+    });
+
+    return { classes, items, styles, prev, next };
   },
 };
 </script>
