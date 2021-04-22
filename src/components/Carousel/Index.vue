7dff658a5228701ec70cb29cd8b7ff221a207bd4
mick xu <mick@holdcode.com>
2021-04-22 18:02:03 +0800

    create carousel files

---
+++ src/components/Carousel/Index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div :class="classes"></div>
+</template>
+
+<script>
+import { computed } from "vue";
+export default {
+  name: "Carousel",
+  props: {},
+  setup() {
+    const classes = computed(() => {
+      return ["carousel"];
+    });
+    return { classes };
+  },
+};
+</script>
+
+<style lang="less" scoped></style>
