a9bffe0f5b9bac2c40cc91cfa6e8d56b713e98fe
mick xu <mick@holdcode.com>
2021-04-22 17:58:25 +0800

    Input demo

---
+++ examples/views/components/InputPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <Article title="Input 输入框">
+    <Row>
+      <Col span="8"><Input v-model="inputValue" /><Code></Code></Col>
+      <Col span="8"></Col>
+      <Col span="8"></Col>
+    </Row>
+  </Article>
+</template>
+
+<script>
+import Article from "@c/Article";
+import Code from "@c/Code";
+import { ref } from "vue";
+export default {
+  name: "InputPage",
+  components: { Article, Code },
+  setup() {
+    const inputValue = ref();
+    return { inputValue };
+  },
+};
+</script>
+
+<style lang="less" scoped></style>
