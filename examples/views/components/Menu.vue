be996764340863d925e45667de6d23e09a82cd5c
mick xu <mick@holdcode.com>
2021-04-25 17:41:11 +0800

    竖排菜单

--- examples/views/components/Menu.vue
+++ examples/views/components/Menu.vue
@@ -2,11 +2,24 @@
   <Article title="Menu 菜单">
     <p></p>
     <section>
-      <Menu>
-        <MenuItem>首页</MenuItem>
-        <MenuItem>新闻</MenuItem>
-        <MenuItem>关于我们</MenuItem>
-      </Menu>
+      <Row>
+        <Col span="24">
+          <Menu>
+            <MenuItem>首页</MenuItem>
+            <MenuItem>新闻</MenuItem>
+            <MenuItem>关于我们</MenuItem>
+          </Menu>
+        </Col>
+      </Row>
+      <Row>
+        <Col span="6">
+          <Menu mode="vertical">
+            <MenuItem>首页</MenuItem>
+            <MenuItem>新闻</MenuItem>
+            <MenuItem>关于我们</MenuItem>
+          </Menu>
+        </Col>
+      </Row>
     </section>
   </Article>
 </template>
