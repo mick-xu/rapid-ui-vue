27151b7517dd7b61bbe2a5adfe14518b1964f33b
mick xu <mick@holdcode.com>
2021-04-28 15:35:12 +0800

    添加 Table 组件演示

---
+++ examples/views/components/Table.vue
@@ -0,0 +1,24 @@
+<template>
+  <Article title="Table 表格">
+    <section>
+      <Table></Table>
+      <h2>示例代码</h2>
+      <Code></Code>
+      <h2>API</h2>
+    </section>
+  </Article>
+</template>
+
+<script>
+import Article from "@c/Article";
+import Code from "@c/Code";
+export default {
+  name: "TablePage",
+  components: { Article, Code },
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="less" scoped></style>
