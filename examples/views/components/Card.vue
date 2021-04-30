9d228bdc6134eb2bf58d47600d78486237716a5c
mick xu <mick@holdcode.com>
2021-04-30 13:28:42 +0800

    增加属性文档

--- examples/views/components/Card.vue
+++ examples/views/components/Card.vue
@@ -13,6 +13,32 @@
         </Col>
         <Col span="12"><Card title="Card 卡片"></Card></Col>
       </Row>
+      <h2>API</h2>
+      <Table>
+        <template #header>
+          <tr>
+            <th>参数</th>
+            <th>说明</th>
+            <th>类型</th>
+            <th>可选值</th>
+            <th>默认值</th>
+          </tr>
+        </template>
+        <tr>
+          <td>bordered</td>
+          <td>是否显示边框</td>
+          <td>Boolean</td>
+          <td>true, false</td>
+          <td>false</td>
+        </tr>
+        <tr>
+          <td>title</td>
+          <td>标题</td>
+          <td>String</td>
+          <td>-</td>
+          <td>-</td>
+        </tr>
+      </Table>
     </section>
   </Article>
 </template>
--- examples/views/components/Switch.vue
+++ examples/views/components/Switch.vue
@@ -6,6 +6,31 @@
         <Switch v-model="switchValue" @onChange="change"></Switch>
       </Space>
       <h2>API</h2>
+      <Table>
+        <template #header>
+          <tr>
+            <th>参数</th>
+            <th>说明</th>
+            <th>类型</th>
+            <th>可选值</th>
+            <th>默认值</th>
+          </tr>
+        </template>
+        <tr>
+          <td>modelValue</td>
+          <td>绑定值</td>
+          <td>Boolean</td>
+          <td>true, false</td>
+          <td>false</td>
+        </tr>
+        <tr>
+          <td>shape</td>
+          <td>开关形状</td>
+          <td>String</td>
+          <td>circle, square</td>
+          <td>circle</td>
+        </tr>
+      </Table>
     </section>
   </Article>
 </template>
