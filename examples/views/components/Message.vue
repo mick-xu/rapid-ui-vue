57b95a2ced338ec2011280457cdeab0ef1905aff
mick xu <mick@holdcode.com>
2021-05-24 17:00:29 +0800

    Message 定时关闭

--- examples/views/components/Message.vue
+++ examples/views/components/Message.vue
@@ -2,10 +2,23 @@
   <Article title="Message 消息">
     <p></p>
     <section>
-      <Button @click="clickHandler">信息</Button>
-      <Button @click="clickHandler">成功</Button>
-      <Button @click="clickHandler">警告</Button>
-      <Button @click="clickHandler">失败</Button>
+      <Row>
+        <Col>
+          <Button @click="clickHandler">默认</Button>
+          <Button @click="clickHandler">信息</Button>
+          <Button @click="clickHandler">成功</Button>
+          <Button @click="clickHandler">警告</Button>
+          <Button @click="clickHandler">失败</Button>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Button @click="clickHandler">信息(背景)</Button>
+          <Button @click="clickHandler">成功(背景)</Button>
+          <Button @click="clickHandler">警告(背景)</Button>
+          <Button @click="clickHandler">失败(背景)</Button>
+        </Col>
+      </Row>
       <Code></Code>
     </section>
   </Article>
@@ -33,7 +46,20 @@ export default {
         case "失败":
           ctx.Message.error("处理失败的消息！");
           break;
+        case "信息(背景)":
+          ctx.Message.info({ message: "系统发来的信息！", fill: true });
+          break;
+        case "成功(背景)":
+          ctx.Message.success({ message: "处理成功的消息！", fill: true });
+          break;
+        case "警告(背景)":
+          ctx.Message.warning({ message: "系统发来的警告信息！", fill: true });
+          break;
+        case "失败(背景)":
+          ctx.Message.error({ message: "处理失败的消息！", fill: true });
+          break;
         default:
+          ctx.Message("默认消息！");
           break;
       }
     };
--- src/Message/Index.vue
+++ src/Message/Index.vue
@@ -1,40 +1,90 @@
 <template>
-  <div :class="classes">
-    <Icon name="info-circle" />
-    <span>{{ message }}</span>
-  </div>
+  <transition name="message" @before-leave="onClose">
+    <div
+      :class="classes"
+      :style="styles"
+      v-show="visible"
+      @mouseenter="clearTimer"
+      @mouseleave="startTimer"
+    >
+      <Icon :name="icon" />
+      <span>{{ message }}</span>
+    </div>
+  </transition>
 </template>
 
 <script>
-import { computed } from "vue";
+import { computed, ref, onMounted } from "vue";
 import Icon from "../Icon";
 const prefixCls = "rapid-message";
 export default {
   name: "Message",
   components: { Icon },
   props: {
+    id: { type: String, default: "" },
     message: {
       type: String,
       default: "",
     },
     type: {
       type: String,
-      default: "info",
+      default: "default",
       validator: (val) => {
-        return ["success", "warning", "info", "error"].includes(val);
+        return ["success", "warning", "info", "error", "default"].includes(val);
       },
     },
+    fill: { type: Boolean, default: false },
+    offset: { type: Number, default: 41 },
+    duration: { type: Number, default: 3000 },
+    onClose: { type: Function, default: () => {} },
   },
   setup(props) {
     const classes = computed(() => {
       return [
         prefixCls,
         "rapid-message-notice",
-        "move-up-leave",
+        { ["rapid-message-default"]: !props.fill },
         `rapid-message-${props.type}`,
+        { [`rapid-message-${props.type}-fill`]: props.fill },
       ];
     });
-    return { classes };
+    const styles = computed(() => {
+      return { top: `${props.offset}px` };
+    });
+    const icons = {
+      info: "info-circle-fill",
+      success: "check-circle-fill",
+      warning: "warning-circle-fill",
+      error: "close-circle-fill",
+      default: "sound-fill",
+    };
+    const icon = computed(() => {
+      return icons[props.type];
+    });
+    const visible = ref(false);
+    let timer = null;
+    const startTimer = () => {
+      if (props.duration > 0) {
+        timer = setTimeout(() => {
+          if (visible.value) {
+            close();
+          }
+        }, props.duration);
+      }
+    };
+    const clearTimer = () => {
+      clearTimeout(timer);
+      timer = null;
+    };
+    const close = () => {
+      visible.value = false;
+    };
+
+    onMounted(() => {
+      startTimer();
+      visible.value = true;
+    });
+    return { classes, styles, icon, visible, startTimer, clearTimer };
   },
 };
 </script>
